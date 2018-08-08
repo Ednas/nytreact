ce95b05c069ee02d9ed6d9e637810fd5163e4978
Ednas <edna@edna.tech>
2018-08-08 18:25:31 -0400

    Added gitignore and set Morgan up for properly logging

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Include Server Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
@@ -11,12 +14,19 @@ var Article = require('./server/model.js');
 var app = express();
 var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
 
+
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+// setup the logger
+app.use(logger('combined', { stream: accessLogStream }))
+
 // Run Morgan for Logging
-app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 app.use(express.static('./public'));
 
@@ -26,12 +36,12 @@ app.use(express.static('./public'));
 mongoose.connect('mongodb://admin:reactrocks@ds023593.mlab.com:23593/heroku_pg676kmk');
 var db = mongoose.connection;
 
-db.on('error', function (err) {
-	console.log('Mongoose Error: ', err);
+db.on('error', function(err) {
+    console.log('Mongoose Error: ', err);
 });
 
-db.once('open', function () {
-	console.log('Mongoose connection successful.');
+db.once('open', function() {
+    console.log('Mongoose connection successful.');
 });
 
 
@@ -39,62 +49,60 @@ db.once('open', function () {
 // -------------------------------------------------
 
 // Main Route
-app.get('/', function(req, res){
-	res.sendFile('./public/index.html');
+app.get('/', function(req, res) {
+    res.sendFile('./public/index.html');
 })
 
 // Route to get all saved articles
 app.get('/api/saved', function(req, res) {
 
-	Article.find({})
-		.exec(function(err, doc){
+    Article.find({})
+        .exec(function(err, doc) {
 
-			if(err){
-				console.log(err);
-			}
-			else {
-				res.send(doc);
-			}
-		})
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(doc);
+            }
+        })
 });
 
 // Route to add an article to saved list
-app.post('/api/saved', function(req, res){
-	var newArticle = new Article(req.body);
+app.post('/api/saved', function(req, res) {
+    var newArticle = new Article(req.body);
 
-	console.log(req.body)
+    console.log(req.body)
 
-	var title = req.body.title;
-	var date = req.body.date;
-	var url = req.body.url;
+    var title = req.body.title;
+    var date = req.body.date;
+    var url = req.body.url;
 
-	newArticle.save(function(err, doc){
-		if(err){
-			console.log(err);
-		} else {
-			res.send(doc._id);
-		}
-	});
+    newArticle.save(function(err, doc) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(doc._id);
+        }
+    });
 });
 
 // Route to delete an article from saved list
-app.delete('/api/saved/', function(req, res){
+app.delete('/api/saved/', function(req, res) {
 
-	var url = req.param('url');
+    var url = req.param('url');
 
-	Article.find({"url": url}).remove().exec(function(err, data){
-		if(err){
-			console.log(err);
-		}
-		else {
-			res.send("Deleted");
-		}
-	});
+    Article.find({ "url": url }).remove().exec(function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send("Deleted");
+        }
+    });
 });
 
 
 // -------------------------------------------------
 
 app.listen(PORT, function() {
-	console.log("App listening on PORT: " + PORT);
+    console.log("App listening on PORT: " + PORT);
 });
